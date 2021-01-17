590de0839f264456a31dbe3c6608ace0901147d4
Gonzalo Ariel Torres Moya <gtorresmoya@gmail.com>
2021-01-16 21:56:46 -0300

    Variable de entorno MONGODB

--- server/config/config.js
+++ server/config/config.js
@@ -1,21 +1,21 @@
 // =================
-//      Puerto
+//  Puerto
 // =================
 process.env.PORT = process.env.PORT || 3000;
 
 
 // =================
-//      Entorno
+//  Entorno
 // =================
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 // =================
-//      Base de Datos
+//  Base de Datos
 // =================
 let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://mgdb:zZNCRO9y2UKGpcX4@cluster0.1jymx.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URL_DB = urlDB;
