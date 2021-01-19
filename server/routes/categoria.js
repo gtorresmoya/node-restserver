139491162db037719017928cfa0dd73cb8dbfb31
Gonzalo Ariel Torres Moya <gtorresmoya@gmail.com>
2021-01-18 22:43:37 -0300

    CRUD Categorias

---
+++ server/routes/categoria.js
@@ -0,0 +1,144 @@
+const express = require('express');
+const _ = require('underscore');
+let { verificaToken, verificaAdminRole } = require('../middlewares/authentication');
+
+const app = express();
+
+let Categoria = require('../models/categoria');
+
+//===============================
+// Mostrar Todas las categorias
+//===============================
+app.get('/categoria', (req, res) => {
+    let desde = req.query.desde || 0;
+    let limitePagina = req.query.limit || 5;
+    desde = Number(desde);
+    limitePagina = Number(limitePagina);
+    Categoria.find({})
+        .skip(desde)
+        .limit(limitePagina)
+        .exec((err, categorias) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            Categoria.countDocuments({}, (err, conteo) => {
+                if (err) {
+                    return res.status(400).json({
+                        ok: false,
+                        err
+                    });
+                }
+
+                res.json({
+                    ok: true,
+                    cuantos: conteo,
+                    categorias
+                });
+            });
+        });
+});
+
+//===============================
+// Mostrar una categoria por id
+//===============================
+app.get('/categoria/:id', verificaToken, (req, res) => {
+    let id = req.params.id;
+    Categoria.findById(id, (err, categoriaDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+        res.json({
+            ok: true,
+            categoria: categoriaDB
+        });
+    });
+});
+
+//===============================
+// Crear nueva categoria
+//===============================
+app.post('/categoria', verificaToken, (req, res) => {
+    let body = req.body;
+    let categoria = new Categoria({
+        descripcion: body.descripcion,
+        usuario: req.usuario._id
+    });
+    categoria.save((err, categoriaDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!categoriaDB) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+        res.json({
+            ok: true,
+            categoria: categoriaDB
+        });
+    });
+});
+
+//===============================
+// Actualizar una categoria por id
+//===============================
+app.put('/categoria/:id', verificaToken, (req, res) => {
+    let id = req.params.id;
+    let body = _.pick(req.body, ['descripcion']);
+    Categoria.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, categoriaDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+        res.json({
+            ok: true,
+            categoria: categoriaDB
+        });
+    });
+});
+
+//===============================
+// Eliminar una categoria por id
+//===============================
+app.delete('/categoria/:id', [verificaToken, verificaAdminRole], (req, res) => {
+    let id = req.params.id;
+    Categoria.findByIdAndRemove(id, (err, categoriaBorrado) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+        if (!categoriaBorrado) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Categoria no encontrada'
+                }
+            });
+        }
+
+        res.json({
+            ok: true,
+            message: 'Categoria borrada'
+        });
+
+    });
+});
+
+
+module.exports = app;
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use(require('./usuarios'));
+app.use(require('./categoria'));
 app.use(require('./login'));
 
 module.exports = app;
